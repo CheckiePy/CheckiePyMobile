69adf98baa441b8b872a00f14afe425b20343ec2
Artem Ustimov <art@ustimov.org>
2017-08-19 13:45:56 +0300

    Fixed network service and added network logger

--- CheckiePyMobile/CheckiePyMobile/Services/NetworkService.cs
+++ CheckiePyMobile/CheckiePyMobile/Services/NetworkService.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using CheckiePyMobile.Helpers;
 using CheckiePyMobile.Models;
 using Newtonsoft.Json;
 
@@ -11,7 +12,7 @@ namespace CheckiePyMobile.Services
 {
     public class NetworkService
     {
-        private HttpClient _client = new HttpClient();
+        private readonly HttpClient _client;
 
         private string _baseUrl = "http://acs.uplatform.ru/api";
 
@@ -25,32 +26,31 @@ namespace CheckiePyMobile.Services
             set
             {
                 _token = value;
-                //_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", _token);
+                _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", _token);
             }
         }
 
         protected NetworkService()
         {
-
+            _client = new HttpClient(new LoggingHandler(new HttpClientHandler()));
         }
 
         public async Task<ResponseModel<List<RepositoryModel>>> GetRepositoriesAsync()
         {
-            return await GetAsync<ResponseModel<List<RepositoryModel>>>("/repository/list");
+            return await GetAsync<ResponseModel<List<RepositoryModel>>>("/repository/list/");
         }
 
         public async Task<ResponseModel<List<CodeStyleModel>>> GetCodeStylesAsync()
         {
-            return await GetAsync<ResponseModel<List<CodeStyleModel>>>("/code_style/list");
+            return await GetAsync<ResponseModel<List<CodeStyleModel>>>("/code_style/list/");
         }
 
         private async Task<T> GetAsync<T>(string url) where T : class 
         {
             try
             {
-                var response = await _client.GetAsync(new Uri("https://requestb.in/wt7owrwt"));
-                var content = await response.Content.ReadAsStringAsync();
-                var json = await _client.GetStringAsync($"{_baseUrl}{url}");
+                var response = await _client.GetAsync($"{_baseUrl}{url}");
+                var json = await response.Content.ReadAsStringAsync();
                 return JsonConvert.DeserializeObject<T>(json);
             }
             catch (Exception e)
