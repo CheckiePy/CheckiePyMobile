6743928973ac680149e9f9fdbb9a4ca61ca3a41a
Artem Ustimov <art@ustimov.org>
2017-08-24 00:07:31 +0300

    Updated layout colors

--- CheckiePyMobile/CheckiePyMobile/Models/RepositoryModel.cs
+++ CheckiePyMobile/CheckiePyMobile/Models/RepositoryModel.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsConnected { get; set; }
+        public bool IsDisconnected => !IsConnected;
     }
 }
